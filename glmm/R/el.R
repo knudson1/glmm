e8ffa8bc33955562581bc27349edca8967e5f849
Christina Knudson <christina@umn.edu>
2014-08-01 14:54:54 -0500

    Moved cumulant and derivs to C.

--- glmm/R/el.R
+++ glmm/R/el.R
@@ -1,12 +1,36 @@
 el <-
 function(Y,X,eta,family.mcml){
 	family.mcml<-getFamily(family.mcml)
-	value<-Y%*%eta-sum(family.mcml$cum(eta))
-	
-	mu<-family.mcml$cp(eta)
+	neta<-length(eta)
+
+	if(family.mcml$family.glmm=="bernoulli.glmm"){
+		foo<-.C("cum3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(1),cumout=double(neta))$cumout
+		mu<-.C("cp3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(1),cpout=double(neta))$cpout
+		cdub<-.C("cpp3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(1),cppout=double(neta))$cppout
+	}
+	if(family.mcml$family.glmm=="poisson.glmm"){
+		foo<-.C("cum3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(2),cumout=double(neta))$cumout
+		mu<-.C("cp3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(2),cpout=double(neta))$cpout
+		cdub<-.C("cpp3",eta=as.double(eta),neta=as.integer(neta),type=as.integer(2),cppout=double(neta))$cppout
+	}
+
+
+#	for(i in 1:length(eta)){
+#		etai<-eta[i]
+#		if(family.mcml$family.glmm=="bernoulli.glmm"){
+#			foo[i]<-.C("cum2", eta = as.double(etai), type = as.integer(1),  cumout=double(1))$cumout
+#			mu[i]<-.C("cp2",eta=as.double(etai),type=as.integer(1),cpout=double(1))$cpout
+#			cdub[i]<-.C("cpp2",eta=as.double(etai),type=as.integer(1),cppout=double(1))$cppout
+#		}
+#		if(family.mcml$family.glmm=="poisson.glmm"){
+#			foo[i]<-.C("cum2", eta = as.double(etai), type = as.integer(2),  cumout=double(1))$cumout
+#			mu[i]<-.C("cp2",eta=as.double(etai),type=as.integer(2),cpout=double(1))$cpout
+#			cdub[i]<-.C("cpp2",eta=as.double(etai),type=as.integer(2),cppout=double(1))$cppout
+#		}
+#	}
+	value<-Y%*%eta-sum(foo)
 	gradient<-t(X)%*%(Y-mu)
 	
-	cdub<-as.vector(family.mcml$cpp(eta))
 	cdubmat<-diag(cdub)
 	hessian<-t(X)%*%(-cdubmat)%*%X
 	
